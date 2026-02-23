4b6ce6d6b90e28c8648f87e964b8655549f525eb
PratikshaJadhav23 <jadhav23pratiksha@gmail.com>
2026-02-23 17:32:57 +0530

    Initial project Setup

---
+++ src/test/java/com/exe/demo/SmartQueueManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartQueueManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
