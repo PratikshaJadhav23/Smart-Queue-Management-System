bedbd02016f781b0cc07bb7f6d817387b7fbca14
PratikshaJadhav23 <jadhav23pratiksha@gmail.com>
2026-02-24 13:01:25 +0530

    Model and Entity are created

---
+++ src/main/java/com/exe/demo/model/Customer.java
@@ -0,0 +1,64 @@
+package com.exe.demo.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "customers")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String fullName;
+	
+	@Column(nullable=false,unique=true)
+	private String email;
+	
+	@Column(nullable=false)
+	private String password;
+	private String Address;
+	private LocalDateTime createdTime;
+	public Customer () {
+		this.createdTime = LocalDateTime.now();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public LocalDateTime getCreatedTime() {
+		return createdTime;
+	}
+	public void setCreatedTime(LocalDateTime createdTime) {
+		this.createdTime = createdTime;
+	}
+	
+}
